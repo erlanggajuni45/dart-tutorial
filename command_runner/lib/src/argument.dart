75d91fcb1c552a2a7c6358b72f2f5687a6728a83
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-03-25 11:11:40 +0700

    define argument abstract class

---
+++ command_runner/lib/src/argument.dart
@@ -0,0 +1,11 @@
+enum OptionType {flag, option}
+
+abstract class Argument {
+  String get name;
+  String? get help;
+
+  Object? get defaultValue;
+  String? get valueHelp;
+
+  String get usage;
+}
