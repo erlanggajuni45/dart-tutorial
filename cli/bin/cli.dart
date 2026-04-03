ba41ecbe1ace230ed4155180c160f02b417eba2a
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-04-04 02:22:39 +0700

    add onOutput argument to cli dart

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -3,7 +3,9 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1';
 
 void main(List<String> arguments) {
-  var commandRunner = CommandRunner(onError: (Object error) {
+  var commandRunner = CommandRunner(onOutput: (String output) async {
+    await write(output);
+  }, onError: (Object error) {
     if (error is Error) {
       throw error;
     }
